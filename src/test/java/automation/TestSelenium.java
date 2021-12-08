9c78ce0a6caa67181f5cfb7c6c0f31e88490fb5a
pnimje <P@ssw0rd>
2021-12-08 20:33:27 +0530

    Testing selenium code

---
+++ src/test/java/automation/TestSelenium.java
@@ -0,0 +1,19 @@
+package automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestSelenium {
+
+    public static void main(String args[]) throws InterruptedException {
+        String baseDir = System.getProperty("user.dir");
+        System.setProperty("webdriver.chrome.driver",baseDir + "\\src\\main\\resources\\drivers\\chromedriver_win.exe");
+        WebDriver driver = new ChromeDriver();
+        Thread.sleep(2000);
+        driver.get("https://www.calculator.net/percent-calculator.html");
+
+        Thread.sleep(5000);
+        driver.close();
+    }
+
+}
