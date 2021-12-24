405d5122fd80de3545d3e3cb206b97df93e31b59
Pranav <pranav.patil0596@gmail.com>
2021-12-24 21:48:59 +0530

    Amazon mobile price and Name update

--- src/test/java/automation/Amazon.java
+++ src/test/java/automation/Amazon.java
@@ -6,10 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 public class Amazon {
     public static void main(String[] args) throws InterruptedException {
@@ -25,31 +22,53 @@ public class Amazon {
 //        WebElement price= driver.findElement(By.xpath((//*[@id="search"]/div[1]/div/div/span[3]/div[2]/div/div/span/descendant::div/descendant::a/descendant::span[1]/descendant::span[2]//descendant::span[2])));
 
         List<WebElement> price= driver.findElements(By.xpath("//*[@id=\"search\"]/div[1]/div/div/span[3]/div[2]/div/div/span/descendant::div/descendant::a/descendant::span[1]/descendant::span[2]//descendant::span[2]/ancestor::span[1]/child::span[2]"));Thread.sleep(10000);
-
-        for(WebElement element : driver.findElements(By.xpath("//*[@id=\"search\"]/div[1]/div/div/span[3]/div[2]/div/div/span/descendant::div/descendant::a/descendant::span[1]/descendant::span[2]//descendant::span[2]/ancestor::span[1]/child::span[2]"))) {
-            System.out.println(element.getText());
-
-        }
-
-
-
-
-        List<WebElement> Name= driver.findElements(By.xpath("//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div/div/span/descendant::h2/descendant::span"));
-
-        for(WebElement element : driver.findElements(By.xpath("//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div/div/span/descendant::h2/descendant::span"))) {
-            System.out.println(element.getText());
-
-        }
-
-//        for(WebElement element : price) {
+//
+//        for(WebElement element : driver.findElements(By.xpath("//*[@id=\"search\"]/div[1]/div/div/span[3]/div[2]/div/div/span/descendant::div/descendant::a/descendant::span[1]/descendant::span[2]//descendant::span[2]/ancestor::span[1]/child::span[2]"))) {
 //            System.out.println(element.getText());
 //
 //        }
 
 
 
-        System.out.println("size of price"+price.size());
-        System.out.println("Size of Name"+Name.size());
+
+        List<WebElement> Name= driver.findElements(By.xpath("//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div/div/span/descendant::h2/descendant::span"));
+
+//        for(WebElement elements : driver.findElements(By.xpath("//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div/div/span/descendant::h2/descendant::span"))) {
+//            System.out.println(elements.getText());
+
+//        }
+
+//        Iterator nam= Name.listIterator();
+//        while (nam.hasNext()){
+//            System.out.println(nam.next().toString());
+//
+//        }
+
+        int count=price.size();
+
+        for(int i=0;i< Name.size();i++){
+            System.out.println("Model-"+""+i+" ---- "+Name.get(i).getText());
+            System.out.println("Price---"+price.listIterator(i).next().getText());
+        }
+
+//        for(int i=0;i< price.size();i++){
+////            System.out.println("Model---"+Name.get(i).getText());
+//            System.out.println("Price---"+price.get(i).getText());
+//        }
+
+
+//        for(WebElement element : driver.findElements(By.xpath("//*[@id=\"search\"]/div[1]/div/div/span[3]/div[2]/div/div/span/descendant::div/descendant::a/descendant::span[1]/descendant::span[2]//descendant::span[2]/ancestor::span[1]/child::span[2]"))) {
+//            System.out.println(element.getText());
+//            for(WebElement elements : driver.findElements(By.xpath("//*[@id=\"search\"]/div[1]/div[1]/div/span[3]/div[2]/div/div/span/descendant::h2/descendant::span"))) {
+//                System.out.println(elements.getText());
+//            }
+//
+//        }
+
+
+
+//        System.out.println("size of price"+price.size());
+//        System.out.println("Size of Name"+Name.size());
 
 
         Thread.sleep(10000);
