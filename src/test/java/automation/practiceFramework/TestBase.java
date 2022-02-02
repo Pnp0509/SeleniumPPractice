f8181b2c0802f8c4407567b2157c3c70b9b82991
prashantnimje <prashantnimje1121@gmail.com>
2022-02-02 20:21:54 +0530

    parallel

--- src/test/java/automation/practiceFramework/TestBase.java
+++ src/test/java/automation/practiceFramework/TestBase.java
@@ -3,13 +3,15 @@ package automation.practiceFramework;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 
 public abstract class TestBase {
 
     public WebDriver driver;
 
-    @BeforeClass(alwaysRun = true)
+    @BeforeTest(alwaysRun = true)
     public void invokeBrowser(){
         System.out.println("Invoking Browser");
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\src\\main\\resources\\drivers\\chromedriver_win.exe");
@@ -18,7 +20,7 @@ public abstract class TestBase {
         System.out.println("Invoking Browser Finished");
     }
 
-    @BeforeClass(alwaysRun = true,dependsOnMethods = "invokeBrowser")
+    @BeforeTest(alwaysRun = true,dependsOnMethods = "invokeBrowser")
     public void baseNavigation(){
         System.out.println("Navigating to Base URL");
         driver.get("https://demo.guru99.com/test/newtours/index.php");
@@ -27,7 +29,7 @@ public abstract class TestBase {
 
 
 
-    @AfterClass(alwaysRun = true)
+    @AfterTest(alwaysRun = true)
     public void closeBrowser(){
         System.out.println("Closing Browser");
         driver.quit();
