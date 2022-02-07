7e00023b8cbe46fdb478ce7bb2747c89926c35bc
prashantnimje <prashantnimje1121@gmail.com>
2022-02-07 20:47:26 +0530

    listener2

--- src/test/java/automation/testNGPractice/ListenerTest2.java
+++ src/test/java/automation/testNGPractice/ListenerTest2.java
@@ -6,13 +6,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-//@Listeners(value = {ListenerITestListener.class,ListenerISuiteListener.class,ListenerIInvokedMethodListener.class})
-//@Listeners(value = ListenerITestListener.class)
-//@Listeners(value = ListenerISuiteListener.class)
-//@Listeners(value = ListenerIInvokedMethodListener.class)
-//@Listeners(value = ListenerRetry.class)  //This wont work
-//@Listeners(value = ListenerAnnotationTransformer.class)  //This wont work
-public class ListenerTest1 {
+public class ListenerTest2 {
     @BeforeClass
     public void beforeClass(){
         System.out.println("I am from before class annotation");
@@ -23,12 +17,12 @@ public class ListenerTest1 {
         System.out.println("I am from after class annotation");
     }
 
-    @Test
+    //@Test
     public void testMethod1(){
         System.out.println("I am testMethod1 From ListnerTest1.");
     }
 
-    @Test()
+    @Test(enabled = false)
     public void testMethodPass(){
         System.out.println("I am testMethodPass From ListnerTest1.");
         Assert.assertTrue(true,"I will never fail testMethodPass From ListnerTest1 ");
